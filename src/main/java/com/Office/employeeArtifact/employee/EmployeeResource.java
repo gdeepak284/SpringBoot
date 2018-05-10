26c0eabc03675832792a85486faa876a8a96d908
gdeepak <gdeepak284@gmail.com>
2018-05-10 23:55:32 +0530

    Added dependencies and repo

--- src/main/java/com/Office/employeeArtifact/employee/EmployeeResource.java
+++ src/main/java/com/Office/employeeArtifact/employee/EmployeeResource.java
@@ -1,8 +1,22 @@
 package com.Office.employeeArtifact.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 public class EmployeeResource {
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @GetMapping("/students/{id}")
+    public Employee retrieveEmployee(@PathVariable long id) {
+        Optional<Employee> student = employeeRepository.findById(id);
+
+        if (!student.isPresent())
+            throw new EmployeeNotFoundException("id-" + id);
+
+        return student.get();
+    }
 }

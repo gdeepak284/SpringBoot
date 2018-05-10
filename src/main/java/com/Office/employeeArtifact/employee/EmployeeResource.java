65f0b88a2e52e4edbb6e1e58671a8da6aa132785
gdeepak <gdeepak284@gmail.com>
2018-05-10 23:59:46 +0530

    Added create, delete and alter methods

--- src/main/java/com/Office/employeeArtifact/employee/EmployeeResource.java
+++ src/main/java/com/Office/employeeArtifact/employee/EmployeeResource.java
@@ -1,22 +1,53 @@
 package com.Office.employeeArtifact.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 public class EmployeeResource {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @GetMapping("/students/{id}")
+    @GetMapping("/employees/{id}")
     public Employee retrieveEmployee(@PathVariable long id) {
-        Optional<Employee> student = employeeRepository.findById(id);
+        Optional<Employee> employee = employeeRepository.findById(id);
 
-        if (!student.isPresent())
+        if (!employee.isPresent())
             throw new EmployeeNotFoundException("id-" + id);
 
-        return student.get();
+        return employee.get();
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public void deleteEmployee(@PathVariable long id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @PostMapping("/employees")
+    public ResponseEntity<Object> createEmployee(@RequestBody Employee employee) {
+        Employee savedEmployee = employeeRepository.save(employee);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedEmployee.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Object> updateEmployee(@RequestBody Employee employee, @PathVariable long id) {
+
+        Optional<Employee> employeeOptional = employeeRepository.findById(id);
+
+        if (!employeeOptional.isPresent())
+            return ResponseEntity.notFound().build();
+
+        employee.setId(id);
+
+        employeeRepository.save(employee);
+
+        return ResponseEntity.noContent().build();
     }
 }

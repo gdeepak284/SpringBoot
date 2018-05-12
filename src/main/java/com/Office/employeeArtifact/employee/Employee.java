7e8aec73fbf7f89b1e6a99d82b74ed83fa180f98
gdeepak <gdeepak284@gmail.com>
2018-05-12 19:41:56 +0530

    added props

--- src/main/java/com/Office/employeeArtifact/employee/Employee.java
+++ src/main/java/com/Office/employeeArtifact/employee/Employee.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name = "employee")
 public class Employee {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     @Column(name = "passportnumber")
